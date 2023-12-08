172ec939204759bca25fd8447914e24452bec73d
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2023-12-08 19:59:29 +0100

    Day 8 part 1

---
+++ day8/day8part1.py
@@ -0,0 +1,20 @@
+import re
+with open("day8/day8input.txt", "r") as file:
+    values = {"T": 10, "J": 11, "Q": 12, "K": 13}
+
+    lines = file.readlines()
+    hmap = {}
+    current = "AAA"
+    for line in lines:
+        row = re.sub("[^a-zA-Z ]", "", line).split()
+        if len(row) == 3:
+            hmap[row[0]] = {"L": row[1], "R": row[2]}
+    count = 0
+    while current != "ZZZ":
+        for char in lines[0].strip():
+            count += 1
+            current = hmap[current][char]
+print(current)
+print(count)
+
+# print(hands)
