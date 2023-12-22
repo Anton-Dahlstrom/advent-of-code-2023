7da59180a391595096b7827585a11b0d052078e4
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2023-12-22 18:05:20 +0100

    Day 13 part 1 solution

--- day13/day13part1.py
+++ day13/day13part1.py
@@ -3,43 +3,70 @@
 # Needs to start at the correct goal, change index that we turned broken into a working spring and keep going until it fails the first time.
 # Then we look at the previous goal until there are no more.
 
+def verticalSearch(arrays, index):
+    converted = ""
+    for array in arrays:
+        converted += array[index]
+    return converted
+
+total = 0
+count = 0
 with open("day13/day13input.txt", "r") as file:
     lines = file.readlines()
+    patterns = []
+    temp = []
     for i in range(0, len(lines)):
         lines[i] = lines[i].strip()
-    failed = False
-    for i in range(0, len(lines)):
-        print(i)
-        print(lines[i])
-        if lines[i] == lines[i-1]:
-            l = i-1
-            r = i
-            while l >= 0 and r < len(lines):
-                if lines[l] == lines[r]:
-                    l -= 1
-                    r += 1
-                else:
-                    failed = True
-                    print("no match")
-                    print(lines[l], lines[r])
-                    print(l, r)
-                    break
-            if not failed:
-                print("match", i)
-                break
-            else:
-                failed = False
+        if lines[i]:
+            temp.append(lines[i])
+            if i == len(lines) -1:
+                patterns.append(temp)
+        else:
+            patterns.append(temp)
+            temp = []
 
-    
-    previous = ""
-    current = ""
-    for j in range(0, len(lines[0])):
-        for i in range(0, len(lines)):
-            current += lines[i][j]
-        # print(current)
-        if current == previous:
-            print("match", j)
-            print(current)
-            print(previous)
-        previous = current
+    failed = False
+    for pnum, pattern in enumerate(patterns):   
+        for i in range(1, len(pattern)):
+            if pattern[i] == pattern[i-1]:
+                l = i-1
+                r = i
+                while l >= 0 and r < len(pattern):
+                    if pattern[l] == pattern[r]:
+                        l -= 1
+                        r += 1
+                    else:
+                        failed = True
+                        break
+                if not failed:
+                    total += i*100
+                    count += 1
+                    break
+                else:
+                    failed = False
+
+        previous = ""
         current = ""
+        for j in range(0, len(pattern[0])):
+            current = verticalSearch(pattern, j)
+            if current == previous:
+                l = j-1
+                r = j
+                while l >= 0 and r < len(pattern[0]):             
+                    if verticalSearch(pattern, l) == verticalSearch(pattern, r):
+                        l -= 1
+                        r += 1
+                    else:
+                        failed = True
+                        break
+                if not failed:
+                    total += j
+                    count += 1
+                    break
+                else:
+                    failed = False
+            previous = current
+            current = ""
+
+print(total)
+# 33381 too low
