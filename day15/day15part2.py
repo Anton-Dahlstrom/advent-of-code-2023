12705c950b861ce3c675a9586227e2666418a52f
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2023-12-25 22:09:03 +0100

    Day 15 part 2 solution

--- day15/day15part2.py
+++ day15/day15part2.py
@@ -1,11 +1,47 @@
+import re
 
+def hashAlgo(chars):
+    value = 0
+    for char in chars:
+        value += ord(char)
+        value *= 17
+        value %= 256
+    return value
 with open("day15/day15input.txt", "r") as file:
     lines = file.readlines()
+    labelArray = []
     for i in range(len(lines)):
-        lines = lines[i].strip().split(",")
+        labelArray += lines[i].strip().split(",")
+    boxes = {}
+    for slot in labelArray:
+        input = re.split("=|-", slot)
+        label = input[0]
+        boxIndex = hashAlgo(label)
+        operator = slot[len(label)]
+        if operator == "=":
+            value = int(slot[-1])
+            if boxIndex in boxes:
+                found = False
+                for i in range(len(boxes[boxIndex])):
+                    if label == boxes[boxIndex][i][0]:
+                        boxes[boxIndex][i][1] = value
+                        found = True
+                if not found:
+                    boxes[boxIndex].append([label, value])
+            else:
+                boxes[boxIndex] = [[label, value]]
+            continue
+        if boxIndex in boxes:
+            array = boxes[boxIndex]
+            for i in range(len(array)):
+                if label == boxes[boxIndex][i][0]:
+                    boxes[boxIndex].pop(i)
+                    break
+
     total = 0
-    for slot in lines:     
-        label = slot[0:2]
-        operator = slot[2]
-        if slot[2] == "=":
-            value = slot[3]
+    for key in boxes:
+        if boxes[key]:
+            for i in range(len(boxes[key])):
+                temp = (key+1) * (i+1) * boxes[key][i][1]
+                total += temp  
+    print(total)
