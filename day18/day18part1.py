24cae9217acd1ac3673d64752fcedb92bf19df1f
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2023-12-27 22:19:08 +0100

    Working on day 18 part 1

--- day18/day18part1.py
+++ day18/day18part1.py
@@ -5,7 +5,7 @@ def addRight(arrays, length):
 
 def addLeft(arrays, length):
     for i in range(len(arrays)):
-        arrays[0] = ["."]*length + arrays[0]
+        arrays[i] = ["."]*length + arrays[i]
     return arrays
 
 def addUp(arrays, length):
@@ -33,7 +33,6 @@ with open("day18/day18input.txt", "r") as file:
     for direction in directions:
         dir = direction[0]
         meter = int(direction[1])
-        
         if dir == "R":
             if x + meter > len(grid[0]):
                 difference = (x + 1 + meter) - len(grid[0])
@@ -44,38 +43,40 @@ with open("day18/day18input.txt", "r") as file:
             
         if dir == "L":
             if x - meter < 0:
-                difference = meter - x + 1
+                difference = meter - x
                 grid = addLeft(grid, difference)
-                x += difference - 1
+                x += difference
             for i in range(meter):
                 x -= 1
                 grid[y][x] = "#"
 
         if dir == "U":
             if y - meter < 0:
-                difference = 0 + (y - meter)
+                difference = meter - y
                 grid = addUp(grid, difference)
                 y += difference
-            print(y)
             for i in range(meter):
                 y -= 1
                 grid[y][x] = "#"
         
         if dir == "D":
-            if y + meter > len(grid):
-                difference = (y + 1 + meter) - len(grid)
+            if y + meter >= len(grid):
+                difference = (y + meter + 1) - len(grid)
                 grid = addDown(grid, difference)
             for i in range(meter):        
                 y += 1
                 grid[y][x] = "#"
+    left = []
+    right = []
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == "#":
+                left.append([i, j])
+                break
+        for k in reversed(range(len(grid))):
+            if grid[i][k] == "#":
+                right.append([i, k])
+                break
 
-    total = 0
-    for g in grid:
-        l = 0
-        r = len(g)-1
-        while g[l] != "#":
-            l +=1
-        while g[r] != "#":
-            r -=1
-        total += (r-l)+1
-    print(total)
+    # 55055 too high
+    # 44184 too high
