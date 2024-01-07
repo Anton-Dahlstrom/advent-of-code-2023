29c3ac231380914ef111a149c0a454a756837651
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-01-07 19:25:08 +0100

    Pseudocode for day 25 part 1

--- day25/day25part1.py
+++ day25/day25part1.py
@@ -1,3 +1,14 @@
+# A function that searches through all paths from a starting node to a goal node.
+# Need to see how many other paths each path kills
+# If a path kills two other paths that would otherwise co-exist it can't be an optimal route
+# How do I find the most paths without overlaps?
+
+# The recursive function that searches through all the nodes has to remember where it's already been and what the goal is.
+
+def searchNode(node, goal, visited):
+    pass
+
+
 with open("day25/day25input.txt", "r") as file:
     lines = file.readlines()
     lines = [line.strip() for line in lines]
@@ -16,3 +27,13 @@ with open("day25/day25input.txt", "r") as file:
             if path not in hmap:
                 hmap[path] = {node: 1}   
     print(hmap)
+    
+    for node in hmap:
+        traveled = {}
+        for endpoint in hmap:
+            if node == endpoint:
+                continue
+
+asd = ["j", "b", "c"]
+if "a" in asd:
+    print("hi")
